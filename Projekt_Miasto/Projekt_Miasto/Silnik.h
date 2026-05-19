29327db5887f8b88bfeb94d63ab145f4f340e82c
sowavx <290518@student.pwr.edu.pl>
2026-05-19 19:40:11 +0200

    Update Silnik.h

--- Projekt_Miasto/Projekt_Miasto/Silnik.h
+++ Projekt_Miasto/Projekt_Miasto/Silnik.h
@@ -1,4 +1,22 @@
 #pragma once
-class Silnik {
+#ifndef SILNIK_H
+#define SILNIK_H
 
+#include "../city/Miasto.h"
+
+class Silnik {
+private:
+    Miasto miasto;
+    int aktualnaTura;
+    bool czyDziałaSymulacja;
+
+    void wykonajTure();
+    void wyswietlRaport() const;
+    void pokazMenu();
+
+public:
+    Silnik();
+    void uruchom();
 };
+
+#endif
