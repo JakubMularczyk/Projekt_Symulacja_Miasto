b8e5d8c565b58abf53b8eb14a53ee54b7b45295b
sowavx <290518@student.pwr.edu.pl>
2026-05-19 19:25:48 +0200

    Update Miasto.h

--- Projekt_Miasto/Projekt_Miasto/Miasto.h
+++ Projekt_Miasto/Projekt_Miasto/Miasto.h
@@ -1,5 +1,36 @@
 #pragma once
-class Miasto
-{
+#ifndef Miasta_h
+#define Miasto_h
+
+#include "MenedzerZasobow.h"
+
+class Miasto {
+private:
+    MenedzerZasobow menedzerZasobow;
+    int bezpieczenstwo; // Odpowiednik parametru SECURITY 
+    
+    // Liczniki potrzebne do weryfikacji warunków upadku miasta 
+    int turyBezJedzenia;
+    int turyBezZlota;
+    int turyBezBezpieczenstwa;
+
+public:
+    Miasto() : 
+        bezpieczenstwo(10), 
+        turyBezJedzenia(0), 
+        turyBezZlota(0), 
+        turyBezBezpieczenstwa(0) {}
+
+    // Dostęp do menedżera zasobów przez referencję
+    MenedzerZasobow& getMenedzerZasobow() {
+        return menedzerZasobow;
+    }
+
+    // Gettery i settery dla statystyki bezpieczeństwa
+    int getBezpieczenstwo() const { return bezpieczenstwo; }
+    void zmienBezpieczenstwo(int ilosc) { bezpieczenstwo += ilosc; }
+    
+    // W tym miejscu w pliku .cpp zaimplementujemy później sprawdzanie warunków upadku [cite: 18, 19, 20, 21]
 };
 
+#endif
