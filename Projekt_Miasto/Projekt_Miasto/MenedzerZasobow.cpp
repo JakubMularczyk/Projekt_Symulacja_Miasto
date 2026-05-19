9d417276eb292d1e6859c0de0a35361c35dab8c9
sowavx <290518@student.pwr.edu.pl>
2026-05-19 19:19:00 +0200

    Update MenedzerZasobow.cpp

--- Projekt_Miasto/Projekt_Miasto/MenedzerZasobow.cpp
+++ Projekt_Miasto/Projekt_Miasto/MenedzerZasobow.cpp
@@ -1,6 +1,38 @@
-#include "MenedzerZasobow.h"
+#ifndef MenedzerZasobow_h
+#define MenedzerZasobow_h
+
 #include <map>
-class ResourceManager {
+#include "Zasoby.h"
+
+class MenedzerZasobow {
 private:
-  std:map<
-  
+    std::map<TypZasobu, int> zasoby;
+
+public:
+    MenedzerZasobow() {
+        zasoby[TypZasobu::FOOD] = 0;
+        zasoby[TypZasobu::GOLD] = 0;
+        zasoby[TypZasobu::WOOD] = 0;
+        zasoby[TypZasobu::STONE] = 0;
+    }
+
+    void dodajZasob(TypZasobu typ, int ilosc) {
+        if (ilosc > 0) {
+            zasoby[typ] += ilosc;
+        }
+    }
+
+    bool zuzyjZasob(TypZasobu typ, int ilosc) {
+        if (ilosc > 0 && zasoby[typ] >= ilosc) {
+            zasoby[typ] -= ilosc;
+            return true; 
+        }
+        return false; 
+    }
+
+    int pobierzIloscZasobu(TypZasobu typ) const {
+        return zasoby.at(typ); 
+    }
+};
+
+#endif
