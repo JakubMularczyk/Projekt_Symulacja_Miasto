394f2f32a2520f8551115227860dcaf21f2629d0
JakubMularczyk <290520@student.pwr.edu.pl>
2026-05-20 22:27:11 +0200

    Update MenedzerZasobow.cpp

--- Projekt_Miasto/Projekt_Miasto/MenedzerZasobow.cpp
+++ Projekt_Miasto/Projekt_Miasto/MenedzerZasobow.cpp
@@ -1,3 +1,27 @@
-#include "Zasoby.h"
+#include "MenedzerZasobow.h"
+MenedzerZasobow::MenedzerZasobow() {
+	zasoby[TypZasobu::FOOD] = 0;
+	zasoby[TypZasobu::GOLD] = 0;
+	zasoby[TypZasobu::WOOD] = 0;
+	zasoby[TypZasobu::STONE] = 0;
+}
+void MenedzerZasobow::dodajZasob(TypZasobu typ, int ilosc) {
+	if (ilosc > 0) {
+		zasoby[typ] += ilosc;
+	}
+}
+bool MenedzerZasobow::zuzyjZasob(TypZasobu typ, int ilosc) {
+	if (ilosc > 0 && zasoby[typ] >= ilosc) {
+		zasoby[typ] -= ilosc;
+		return true;
+	}
+	return false;
+}
+int MenedzerZasobow::pobierzIloscZasobu(TypZasobu typ) const {
+	return zasoby.at(typ);
+}
+
+
+
 
 
