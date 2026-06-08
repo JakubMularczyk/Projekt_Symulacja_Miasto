333fb2df7f25b2bb3e488f3efbc44692ef685d8a
sowavx <290518@student.pwr.edu.pl>
2026-06-08 17:59:57 +0200

    Update City.h

--- src/City.h
+++ src/City.h
@@ -3,33 +3,28 @@
 #include <vector>
 #include <memory>
 #include "Citizen.h"
-#include "ResourceManager.h"
-#include "Building.h" 
+#include "Building.h"
+#include "ResourceManager.h" 
 
 class City {
 private:
-    ResourceManager resourceManager;
     int safety;
     int turnsWithoutFood;
     int turnsWithoutGold;
     int turnsWithoutSafety;
-    
     std::vector<std::unique_ptr<Citizen>> citizens;
     std::vector<std::unique_ptr<Building>> buildings;
 
 public:
     City();
 
-    ResourceManager& getResourceManager();
-    const ResourceManager& getResourceManager() const;
     int getSafety() const;
+    void changeSafety(int amount);
+
+    void updateTurnsToCollapse(const ResourceManager& resourceManager);
+    bool checkCollapseConditions();
 
     void addCitizen(std::unique_ptr<Citizen> citizen);
     void executeCitizenActions();
-
     void addBuilding(std::unique_ptr<Building> building);
-
-    void changeSafety(int amount);
-    void updateTurnsToCollapse();
-    bool checkCollapseConditions();
 };
