4fcd9ff66ef90f63748a025ba08720c90260d47d
JustynaPatan <290501@student.pwr.edu.pl>
2026-06-13 21:24:30 +0200

    Przeniesienie dostępu do zasobów do klasy bazowej oraz dodanie globalnej logiki konsumpcji i zadowolenia.

--- src/Citizen.cpp
+++ src/Citizen.cpp
@@ -1,11 +1,38 @@
 #include "Citizen.h"
-#include "ResourceManager.h"
 #include <iostream>
 
-Citizen::Citizen(int happinessLevel, int age, int healthLevel)
-    : happinessLevel(happinessLevel), age(age), healthLevel(healthLevel) {
+Citizen::Citizen(int happinessLevel, int age, int healthLevel, ResourceManager* rm)
+    : happinessLevel(happinessLevel), age(age), healthLevel(healthLevel), resourceManager(rm) {
 }
 
 void Citizen::work() {}
 
-void Citizen::consume() {}
+void Citizen::consume() {
+    if (!resourceManager) return;
+
+    int availableFood = resourceManager->getResourceAmount(ResourceType::FOOD);
+    int availableGold = resourceManager->getResourceAmount(ResourceType::GOLD);
+
+    int highFoodThreshold = 100;
+    int lowFoodThreshold = 40;
+
+    if (availableFood > highFoodThreshold) {
+        resourceManager->consumeResource(ResourceType::FOOD, 2);
+
+        if (availableGold > 50) {
+            happinessLevel += 5;
+        }
+        else {
+            happinessLevel += 2;
+        }
+    }
+    else if (availableFood > lowFoodThreshold) {
+        resourceManager->consumeResource(ResourceType::FOOD, 1);
+    }
+    else {
+        happinessLevel -= 10;
+    }
+
+    if (happinessLevel > 100) happinessLevel = 100;
+    if (happinessLevel < 0) happinessLevel = 0;
+}
--- src/Citizen.h
+++ src/Citizen.h
@@ -1,16 +1,17 @@
 #pragma once
-
+#include "ResourceManager.h"
 class Citizen {
 protected:
     int happinessLevel;
     int age;
     int healthLevel;
+    ResourceManager* resourceManager;
 
 public:
-    Citizen(int happinessLevel = 0, int age = 0, int healthLevel = 0);
+    Citizen(int happinessLevel = 100, int age = 0, int healthLevel = 100, ResourceManager* rm = nullptr);
 
     virtual ~Citizen() = default;
 
     virtual void work() = 0;
-    virtual void consume() = 0;
+    virtual void consume();
 };
