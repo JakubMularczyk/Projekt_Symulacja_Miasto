5a7f9adf1a0b81d892de53c206e257452778933f
JustynaPatan <290501@student.pwr.edu.pl>
2026-06-13 21:43:14 +0200

    Usunięcie niepotrzebnej zmiennej healthLevel z klasy bazowej Citizen

--- src/Citizen.cpp
+++ src/Citizen.cpp
@@ -1,38 +1,11 @@
 #include "Citizen.h"
+#include "ResourceManager.h"
 #include <iostream>
 
-Citizen::Citizen(int happinessLevel, int age, int healthLevel, ResourceManager* rm)
-    : happinessLevel(happinessLevel), age(age), healthLevel(healthLevel), resourceManager(rm) {
+Citizen::Citizen(int happinessLevel, int age)
+    : happinessLevel(happinessLevel), age(age) {
 }
 
 void Citizen::work() {}
 
-void Citizen::consume() {
-    if (!resourceManager) return;
-
-    int availableFood = resourceManager->getResourceAmount(ResourceType::FOOD);
-    int availableGold = resourceManager->getResourceAmount(ResourceType::GOLD);
-
-    int highFoodThreshold = 100;
-    int lowFoodThreshold = 40;
-
-    if (availableFood > highFoodThreshold) {
-        resourceManager->consumeResource(ResourceType::FOOD, 2);
-
-        if (availableGold > 50) {
-            happinessLevel += 5;
-        }
-        else {
-            happinessLevel += 2;
-        }
-    }
-    else if (availableFood > lowFoodThreshold) {
-        resourceManager->consumeResource(ResourceType::FOOD, 1);
-    }
-    else {
-        happinessLevel -= 10;
-    }
-
-    if (happinessLevel > 100) happinessLevel = 100;
-    if (happinessLevel < 0) happinessLevel = 0;
-}
+void Citizen::consume() {}
--- src/Citizen.h
+++ src/Citizen.h
@@ -1,17 +1,15 @@
 #pragma once
-#include "ResourceManager.h"
+
 class Citizen {
 protected:
     int happinessLevel;
     int age;
-    int healthLevel;
-    ResourceManager* resourceManager;
 
 public:
-    Citizen(int happinessLevel = 100, int age = 0, int healthLevel = 100, ResourceManager* rm = nullptr);
+    Citizen(int happinessLevel = 0, int age = 0);
 
     virtual ~Citizen() = default;
 
     virtual void work() = 0;
-    virtual void consume();
+    virtual void consume() = 0;
 };
