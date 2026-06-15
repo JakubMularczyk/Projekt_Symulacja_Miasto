9712e3d6720a892ccb5587f8c654d3f8fa2631ad
JakubMularczyk <290520@student.pwr.edu.pl>
2026-06-15 21:33:43 +0200

    plague oraz riots event dodane

--- src/PopulationFactory.cpp
+++ src/PopulationFactory.cpp
@@ -19,23 +19,25 @@
 #define WOODCUTTER_PRODUCTION 6
 #define STONECUTTER_PRODUCTION 6
 
-#define GUARD_PRODUCTION 10
+#define GUARD_PRODUCTION 5
 #define GUARD_CONSUMPTION 1
 
 
 void PopulationFactory::createStartingPopulation(City& city, ResourceManager& resourceManager) {
+	//Farmers
 	city.getPopulationManager().addCitizen(std::make_unique<Farmer>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, FARMER_PRODUCTION, FARMER_CONSUMPTION));
 	city.getPopulationManager().addCitizen(std::make_unique<Farmer>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, FARMER_PRODUCTION, FARMER_CONSUMPTION));
 	city.getPopulationManager().addCitizen(std::make_unique<Farmer>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, FARMER_PRODUCTION, FARMER_CONSUMPTION));
 	city.getPopulationManager().addCitizen(std::make_unique<Farmer>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, FARMER_PRODUCTION, FARMER_CONSUMPTION));
 	city.getPopulationManager().addCitizen(std::make_unique<Farmer>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, FARMER_PRODUCTION, FARMER_CONSUMPTION));
-
+	//Merchant
 	city.getPopulationManager().addCitizen(std::make_unique<Merchant>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, MERCHANT_PROD_LOW));
 	city.getPopulationManager().addCitizen(std::make_unique<Merchant>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, MERCHANT_PROD_HIGH));
-
+	//Woodcutters
 	city.getPopulationManager().addCitizen(std::make_unique<Woodcutter>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, WOODCUTTER_PRODUCTION));
+	//Stonecutters
 	city.getPopulationManager().addCitizen(std::make_unique<Stonecutter>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &resourceManager, STONECUTTER_PRODUCTION));
-
+	//Guards
 	city.getPopulationManager().addCitizen(std::make_unique<Guard>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &city, &resourceManager, GUARD_PRODUCTION, GUARD_CONSUMPTION));
 	city.getPopulationManager().addCitizen(std::make_unique<Guard>(HAPPINESS_LEVEL, AGE, HEALTH_LEVEL, &city, &resourceManager, GUARD_PRODUCTION, GUARD_CONSUMPTION));
 
